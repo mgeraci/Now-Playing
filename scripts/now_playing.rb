5f61ac820f2a13c72b56d118ee95c4401fa38c58
Michael P. Geraci <me@mgeraci.com>
2015-04-23 12:29:56 -0400

    update itunes script to not open itunes every minute

--- scripts/now_playing.rb
+++ scripts/now_playing.rb
@@ -15,29 +15,32 @@ end
 f.close
 secret = clean_string secret
 
-# get info from itunes if playing
-if `osascript -e 'tell application "iTunes" to get player state'`.gsub(/\s/, '') == 'playing'
-  artist = clean_string `osascript -e 'tell application "iTunes" to get artist of current track'`
-  album = clean_string `osascript -e 'tell application "iTunes" to get album of current track'`
-  title = clean_string `osascript -e 'tell application "iTunes" to get name of current track'`
+if `osascript -e 'application "iTunes" is running'`
+	# get info from itunes if playing
+	if `osascript -e 'tell application "iTunes" to get player state'`.gsub(/\s/, '') == 'playing'
+		artist = clean_string `osascript -e 'tell application "iTunes" to get artist of current track'`
+		album = clean_string `osascript -e 'tell application "iTunes" to get album of current track'`
+		title = clean_string `osascript -e 'tell application "iTunes" to get name of current track'`
 
-  puts artist
-  puts album
-  puts title
-  puts secret
+		puts artist
+		puts album
+		puts title
+		puts secret
 
-  # assemble the url
-  if ARGV.to_s == "test" #|| 1 == 1
-    domain = "http://127.0.0.1:3000"
-  else
-    domain = "http://now-playing.meteor.com"
-  end
-  path = "add_song"
-  path = "#{path}?secret=#{secret}&artist=#{artist}&album=#{album}&title=#{title}"
-  url = URI.parse "#{domain}/#{path}"
+		# assemble the url
+		if ARGV.to_s == "test" #|| 1 == 1
+			domain = "http://127.0.0.1:3000"
+		else
+			domain = "http://now-playing.meteor.com"
+		end
+		path = "add_song"
+		path = "#{path}?secret=#{secret}&artist=#{artist}&album=#{album}&title=#{title}"
+		url = URI.parse "#{domain}/#{path}"
 
-  puts "\nsending the update!"
-  Net::HTTP.get_response url
+		puts "\nsending the update!"
+		Net::HTTP.get_response url
+	else
+		puts 'Nothing is on!'
 else
-  puts 'Nothing is on!'
+  puts 'iTunes is not running.'
 end
